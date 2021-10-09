5422ab6ad943a5655073e12e4342cc9d49f42876
Sergey Nazhestkin <9497020@mail.ru>
2021-10-09 18:42:38 +0300

    Add project files.

---
+++ WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsAndAPIs
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            inkRadio.IsChecked = true;
+            comboColors.SelectedIndex = 0;
+        }
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                MyInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fs);
+                MyInkCanvas.Strokes = strokes;
+            }
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            MyInkCanvas.Strokes.Clear();
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string colorToUse = (comboColors.SelectedItem as StackPanel)?.Tag.ToString();
+            MyInkCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as RadioButton)?.Content.ToString())
+            {
+                case "Ink Mode!":
+                    MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Erase Mode!":
+                    MyInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    break;
+                case "Select Mode!":
+                    MyInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+            }
+        }
+    }
+}
---
+++ WpfControlsAndAPIs/MyDoubleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Converters;
+
+namespace WpfControlsAndAPIs
+{
+    public class MyDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+            return (int)v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
